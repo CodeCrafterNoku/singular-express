ca78ae0da9cbcda9884857b158591da606608fc5
Nokubonga Ngema <NNgema@SINGULAR.CO.ZA>
2025-07-02 14:16:07 +0200

    hpapy coding

---
+++ Server/SingularExpress.Api/Services/UserLockoutService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingularExpress.Api.Services
+{
+    public class UserLockoutService
+    {
+        
+    }
+}
--- Server/SingularExpress.Models.Tests/obj/Debug/net8.0/SingularExpress.Models.Tests.AssemblyInfo.cs
+++ Server/SingularExpress.Models.Tests/obj/Debug/net8.0/SingularExpress.Models.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingularExpress.Models.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c632d79bb4a461d7cbb45d6f4d7b677d0bd90e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e030a7b6da138ac23ea2a50c7bec58e5b573d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingularExpress.Models.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingularExpress.Models.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
